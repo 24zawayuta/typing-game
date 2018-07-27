89929c5bff70c4475cdc37067cf2beeea44a9027
24zawayuta <a5816070@aoyama.jp>
2018-07-27 21:58:18 +0900

    change css and add ranking

--- blog/static/css/mainGame.js
+++ blog/static/css/mainGame.js
@@ -2,18 +2,16 @@ var rand = new Array();
 var count = 0;
 var queNum = 5;
 var numnum = 0;
-var stnum=0;
-var finnum=0;
 var total=-1;
-var per=0;
 var sttime=0;
 var fintime=0;
-var leng="";
 var successcount=0;
-var besttime=4000;
+var besttime=new Array(700,700,700);
+var copytime=new Array(700,700,700);
 var newcheck=0;
-var misskeepcount=0;
 var missmax=0;
+var leng="";//計算により生成される文字列の入力
+var rankleng=""
 //キー状態管理変数の定義
 var KEYS = new Array(256);
 //キーの状態を false （押されていない）で初期化
@@ -40,12 +38,7 @@ var Alphabet = new Array("a","b","c","d","e","f","g","h","i",
                          "j","k","l","m","n","o","p","q","r",
                          "s","t","u","v","w","x","y","z");//対応するアルファベット
 var misschar = new Array(0,0,0,0,0);
-var misskeep=new Array(0,0,0,0,0,0,0,0,0,
-                        0,0,0,0,0,0,0,0,0,
-                        0,0,0,0,0,0,0,0);
-var missresult=new Array(-1,-1,-1,-1,-1,-1,-1,-1,-1,
-                        -1,-1,-1,-1,-1,-1,-1,-1,-1,
-                        -1,-1,-1,-1,-1,-1,-1,-1);
+
 
 /*--------------------------------------------*/
 
@@ -83,18 +76,32 @@ function typeGame(){
         if (count < queNum){
             document.getElementById("frame").innerHTML = Alphabet[rand[count]];
         }else{
-			if(total==count){
+			if(total==count){//成功時
 				successcount+=1;
 	            DD2 = new Date();
 				finSeconds = DD2.getSeconds();
-				if(Number(finSeconds)-Number(stSeconds)<besttime){
-					besttime=Number(finSeconds)-Number(stSeconds);
+				for(var j=0;j<3;j++){
+				    copytime[j]=besttime[j];
 				}
-				document.getElementById("success").innerHTML ="<font size=7 color=red>"+successcount+"回目の成功!!</font><br><img src='static/css/success.jpg'><br><font size=7 color=red>besttime:</font><br>"+besttime;
-				document.getElementById("frame").innerHTML="";
-				//document.getElementById("fail").outerHTML = ""
+				for(var j=0;j<3;j++){
+						if(Number(finSeconds)-Number(stSeconds)<=besttime[j]){/*ここ*/
+							besttime[j]=Number(finSeconds)-Number(stSeconds);
+							for(var s=j;s<2;s++){
+								besttime[s+1]=copytime[s];//ランキング書き直し
+							}
+							break;
+						}
+				}
+				for(var j=0;j<3;j++){
+					if(besttime[j]!=700){
+						rankleng+="Rank "+(Number(j)+Number(1))+":"+besttime[j]+";";
+					}
+				}
+				document.getElementById("rank").innerHTML=rankleng;
+				document.getElementById("success").innerHTML =successcount+"回目の成功!!<br>thistime:"+(finSeconds-stSeconds)+"<br><img src='static/css/success.jpg'>";
+				document.getElementById("fail").innerHTML=""; 
 			}
-			else{
+			else{//失敗時
 			    DD2 = new Date();
 				finSeconds = DD2.getSeconds();
 				for(var i=0;i<queNum;i++){
@@ -102,30 +109,17 @@ function typeGame(){
 					 leng+=Alphabet[rand[i]]+',';
 					}
 				}
-				document.getElementById("fail").innerHTML = '<font size=7 color=blue>マッチング率：</font>'+(count*100/total).toFixed(0)+'%<br><font size=7 color=blue>最近間違えたキー:</font>'+leng;
+				document.getElementById("success").innerHTML="";
+				document.getElementById("fail").innerHTML = 'マッチング率：'+(count*100/total).toFixed(0)+'%<br>間違えたキー:'+leng;
 				for(var i=0;i<queNum;i++){
-					if(misschar[i]==1){
-					  misskeep[rand[i]]+=1;
-					}
 					misschar[i]=0;
 				}
-				/*missmax=Math.max.apply(null, misskeep);
-				missresult[0]=misskeep.indexOf(missmax);
-				var maxleng=missresult[0];
-				for(var i=0;i<misskeep.length-1;i++){
-					missresult[i+1]=misskeep.indexOf(missmax,missresult[i]);
-					if(missresult[i+1]==-1){
-						break;
-					}
-					maxleng+=missresult[i+1];
-				}
-				document.getElementById("failcollection").innerHTML = "あなたがよく間違えるのは"+maxleng;
-				*/
 			}
-			total=-1; 
+			total=-1;
 			count=0;
 			numnum=0;
 		    leng="";
+			rankleng="";
 			gameSet();
 			newcheck=0;
         }
@@ -133,11 +127,9 @@ function typeGame(){
     }
 	else{       
          if(newcheck>0){
-		 //document.getElementById("success").innerHTML=count;
          misschar[count]=1;
         }
         newcheck++;
 	}
 }
-
 gameSet();
